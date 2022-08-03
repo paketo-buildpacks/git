aedd7cb48e64980aa22a1df1e41149edc90dc2d7
Forest Eckhardt <forest.eckhardt@gmail.com>
2022-08-03 14:44:48 +0000

    Enables debug logging by respecting BP_LOG_LEVEL

--- run/main.go
+++ run/main.go
@@ -3,16 +3,16 @@ package main
 import (
 	"os"
 
+	"github.com/paketo-buildpacks/git"
 	"github.com/paketo-buildpacks/packit/v2"
 	"github.com/paketo-buildpacks/packit/v2/pexec"
 	"github.com/paketo-buildpacks/packit/v2/scribe"
 	"github.com/paketo-buildpacks/packit/v2/servicebindings"
-	"github.com/paketo-buildpacks/git"
 )
 
 func main() {
 	executable := pexec.NewExecutable("git")
-	emitter := scribe.NewEmitter(os.Stdout)
+	emitter := scribe.NewEmitter(os.Stdout).WithLevel(os.Getenv("BP_LOG_LEVEL"))
 	bindingResolver := servicebindings.NewResolver()
 
 	packit.Run(
